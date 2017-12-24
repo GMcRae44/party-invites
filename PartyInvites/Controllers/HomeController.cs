695a4eb5bd38f2443fa988df3ff651429e4da914
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-24 10:38:23 +0100

    create home view and action

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
@@ -10,23 +7,11 @@ namespace PartyInvites.Controllers
 {
   public class HomeController : Controller
   {
-    public IActionResult Index()
+    public ViewResult Index()
     {
-      return View();
-    }
-
-    public IActionResult About()
-    {
-      ViewData["Message"] = "Your application description page.";
-
-      return View();
-    }
-
-    public IActionResult Contact()
-    {
-      ViewData["Message"] = "Your contact page.";
-
-      return View();
+      int hour = DateTime.Now.Hour;
+      ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+      return View("MyView");
     }
 
     public IActionResult Error()
